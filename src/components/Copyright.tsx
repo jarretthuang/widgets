b1d234be5e693baf12dbd72721f4e08703d5e3fc
Jarrett Huang <jarrett@jhuang.ca>
2023-12-29 01:50:02 -0800

    Add analytics

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,7 +2,7 @@ export default function Copyright() {
   const currentYear = new Date().getFullYear();
   return (
     <a
-      href="https://jhuang.ca"
+      href="https://labs.jhuang.ca"
       target="_blank"
       className="Copyright group flex w-fit cursor-pointer flex-row items-center rounded-3xl bg-black/10 px-8 py-3 font-medium shadow-md backdrop-blur dark:bg-white/20"
     >
