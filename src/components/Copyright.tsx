4a7bf32299a6a1f114e95cfd0daeb6845afc1098
Jarrett Huang <jarrett@jhuang.ca>
2023-12-31 00:26:09 -0800

    Add link to github repo

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,15 +1,28 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 export default function Copyright() {
   const currentYear = new Date().getFullYear();
   return (
-    <a
-      href="https://labs.jhuang.ca"
-      target="_blank"
-      className="Copyright group flex w-fit cursor-pointer flex-row items-center rounded-3xl bg-white/20 px-8 py-3 font-medium shadow-md backdrop-blur"
-    >
-      <span className="select-none whitespace-nowrap text-lg text-black opacity-70 duration-100 group-hover:opacity-90 dark:text-white">
-        <span className="hidden md:inline">Copyright</span> © {currentYear}{" "}
-        Jarrett Huang
+    <div className="Copyright flex w-fit cursor-pointer flex-row items-center rounded-3xl bg-white/20 px-8 py-3 font-medium shadow-md backdrop-blur">
+      <span className="flex select-none whitespace-nowrap text-xl text-gray-800 duration-100 md:text-lg dark:text-gray-100">
+        <span className="hidden md:inline">Copyright</span>
+        <span className="px-2"> © {currentYear} </span>
+        <a
+          className="hover:opacity-80"
+          href="https://labs.jhuang.ca"
+          target="_blank"
+        >
+          <span className="hidden md:inline">Jarrett Huang</span>
+          <span className="inline md:hidden">JH</span>
+        </a>
+        <a
+          className="flex items-center px-2 hover:opacity-80"
+          href="https://github.com/jarretthuang/widgets"
+          target="_blank"
+        >
+          <GitHubIcon fontSize="medium" />
+        </a>
       </span>
-    </a>
+    </div>
   );
 }
