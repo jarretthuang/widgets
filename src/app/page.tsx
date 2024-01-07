5628588c0fda05871ff0ed4a8ea62fb237baa953
Jarrett Huang <jarrett@jhuang.ca>
2024-01-06 23:07:48 -0800

    Better dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="flex h-[70svh] w-full overflow-hidden">
         <a
           href="/finance"
-          className="dark:border-1 m-auto flex h-40 w-40 rotate-[15deg] cursor-pointer select-none rounded-xl bg-lime-100/40 shadow-lg duration-100 hover:bg-lime-100/70 md:h-80 md:w-80 dark:border-stone-500/80 dark:bg-gray-800/60 dark:hover:bg-gray-800/90"
+          className="dark:border-1 m-auto flex h-40 w-40 rotate-[15deg] cursor-pointer select-none rounded-xl bg-lime-100/40 shadow-lg backdrop-blur duration-100 hover:bg-lime-100/70 md:h-80 md:w-80 dark:border-stone-500/80 dark:bg-black/40 dark:hover:bg-black/30"
         >
           <div className="m-auto text-3xl">
             <MonetizationOnIcon fontSize="large" />
--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,8 +3,8 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 export default function Copyright() {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="Copyright flex w-fit cursor-pointer flex-row items-center rounded-3xl bg-white/10 px-8 py-3 font-medium shadow-md backdrop-blur">
-      <span className="flex select-none whitespace-nowrap text-xl text-gray-800 duration-100 md:text-lg dark:text-gray-100">
+    <div className="Copyright flex w-fit cursor-pointer flex-row items-center rounded-3xl bg-white/10 px-8 py-3 font-medium shadow-md backdrop-blur dark:bg-black/20">
+      <span className="flex select-none whitespace-nowrap text-xl text-gray-700 duration-100 md:text-lg dark:text-gray-200">
         <span className="hidden md:inline">Copyright</span>
         <span className="px-2"> © {currentYear} </span>
         <a
