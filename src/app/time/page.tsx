12c624561139123ad494ea576264b218c4a434a0
jarrett.huang <jarrett@jhuang.ca>
2024-02-14 22:51:45 -0800

    Add a page for time

---
+++ src/app/time/page.tsx
@@ -0,0 +1,14 @@
+import HomeLayout from "@/components/HomeLayout";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Widgets | Time",
+  description: "A collection of web widgets for time-related information",
+};
+
+export default function Time() {
+  return (
+    <HomeLayout title="Time">
+      <div>Time</div>
+    </HomeLayout>
+  );
+}
